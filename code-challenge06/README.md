4a2fbbdec97d41b0d718531a41dd4bfa12acb847
mariamalshammari <malshammari37@gmail.com>
2021-08-23 19:30:17 +0300

    linkedListInsertions

---
+++ code-challenge06/README.md
@@ -0,0 +1,15 @@
+# Challenge Summary
+<!-- Description of the challenge -->
+We need Extend a Linked List to allow various insertion methods(append,insert before ,insert after).
+
+
+## Whiteboard Process
+<!-- Embedded whiteboard image -->
+![linked-list-insertion](/code-challenge06/linked-list-insertion.png)
+
+## Solution
+<!-- Show how to run your code, and examples of it in action -->
+1-create a Node class that has properties for the value stored in the Node, and a pointer to the next Node. 
+2- Create a Linked List class
+3- Create amethod of insertion on Linked List class
+4- (append,insert before ,insert after)
--- code-challenge06/linked-list-insertion.js
+++ code-challenge06/linked-list-insertion.js
@@ -5,7 +5,8 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
-    insert(value) {
+  
+    append(value) {
         const node = new Node(value);
         if (!this.head) {
             this.head = node;
@@ -17,11 +18,58 @@ class LinkedList {
 
         return this;
     }
-    
-    
 
-  
+    insertBefore(value, newValue) {
+        let newNode = this.head;
+        const node = new Node(newValue);
+        if (newNode.value === value) {
+            node.next = this.head;
+            this.head = node;
+            // return this;
+        }
+        else {
+            while (newNode.next) {
+                if (newNode.next.value === value) {
+                    node.next = newNode.next;
+                    newNode.next = node;
+                
+                // return this;
+                break;
+            }
+        else {
+                newNode = newNode.next;
+            }
+
+        }
+
+
+    }}
+
+    insertAfter(value, newValue) {
+        let newNode = this.head;
+        const node = new Node(newValue);
+       
+        
+            while (newNode) {
+                if (newNode.value === value) {
+                    node.next = newNode.next;
+                    newNode.next = node;
+                    
+                break;
+            }
+         
+                newNode = newNode.next;
+            
+
+        
+
+
+    }}
+
+
+
 
 
 }
+    
 module.exports = LinkedList;
