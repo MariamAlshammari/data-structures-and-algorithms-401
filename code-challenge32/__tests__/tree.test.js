54a61740d3ec13a0d3e0cd5252a16c1b0c169eca
mariamalshammari <malshammari37@gmail.com>
2021-09-27 18:50:03 +0300

    tree=intersection

---
+++ code-challenge32/__tests__/tree.test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+// const breadthFirst = require('../tree-breadth-first');
+const BinaryTree = require('../binaryTree')
+const Node = require('../node');
+
+describe('binary tree', () => {
+    let tree = null;
+    beforeAll(() => {
+        let one = new Node(1);
+        let two = new Node(2);
+        let three = new Node(3);
+        let four = new Node(4);
+        let five = new Node(5);
+        let six = new Node(6);
+        let seven = new Node(7);
+        let eight = new Node(8);
+        let nine = new Node(9);
+
+        one.left = two;
+        one.right = three;
+        two.left = four;
+        two.right = five;
+        three.left = six;
+        three.right = seven
+        four.left = eight;
+        four.right = nine;
+        tree = new BinaryTree(one);
+    });
+    it('preOrder', () => {
+        let preOrder = tree.preOrder();
+        let expected = [1, 2, 4, 8, 9,  5, 3, 6, 7];
+        expect(preOrder).toEqual(expected);
+    });
+    it('inOrder', () => {
+        let inOrder = tree.inOrder();
+        let expected = [8, 4,  9, 2, 5, 1, 6, 3, 7];
+        expect(inOrder).toEqual(expected);
+    });
+
+    it('postOrder', () => {
+        let postOrder = tree.postOrder();
+        let expected = [8,  9, 4, 5, 2, 6, 7, 3, 1];
+        expect(postOrder).toEqual(expected);
+    });
+
+    it('Traverse the input tree using a Breadth-first approach', () => {
+        let expected = [1, 2, 3, 4, 5, 8, 9, 6, 7];
+        expect(breadthFirst(tree)).toEqual(expected);
+    });
+
+
+
+})
